62121a4e3a9c71df7572e5aa1232fbd78a401f8c
Mykola <jacktyler2391@gmail.com>
2024-11-30 17:07:21 +0200

    fix flake8

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 import io
 
+
 def serialize_car_object(car: Car) -> bytes:
+
     serializer = CarSerializer(car)
     json = JSONRenderer().render(serializer.data)
     return json
 
-
 def deserialize_car_object(json: bytes) -> Car:
+
     stream = io.BytesIO(json)
     data = JSONParser().parse(stream)
     serializer = CarSerializer(data=data)
+
     if serializer.is_valid():
          car = serializer.save()
          return car
